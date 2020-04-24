3574fe9d4f42c0971c1e09291d8c1073cf7f4cd2
Mike <michael.hartman0@gmail.com>
2020-04-23 22:15:40 -0400

    Create Semi-Annual Channel (Targeted).sql

---
+++ Semi-Annual Channel (Targeted).sql
@@ -0,0 +1,6 @@
+select
+SMS_R_SYSTEM.ResourceID,SMS_R_SYSTEM.ResourceType,SMS_R_SYSTEM.Name,SMS_R_SYSTEM.SMSUniqueIdentifier,SMS_R_SYSTEM.ResourceDomainORWorkgroup,SMS_R_SYSTEM.Client
+from SMS_R_System
+inner join SMS_G_System_OFFICE365PROPLUSCONFIGURATIONS
+on SMS_G_System_OFFICE365PROPLUSCONFIGURATIONS.ResourceID = SMS_R_System.ResourceId
+where SMS_G_System_OFFICE365PROPLUSCONFIGURATIONS.CDNBaseUrl = "http://officecdn.microsoft.com/pr/b8f9b850-328d-4355-9145-c59439a0c4cf"
